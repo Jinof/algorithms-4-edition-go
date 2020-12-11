d6ca7d54796ee73b3727c9c1c16006e76b13e89c
Jinof <Jinof@foxmail.com>
2020-12-11 16:32:55 +0800

    :pencil2:

--- msort/select.go
+++ msort/select.go
@@ -3,7 +3,7 @@ package msort
 // SelectSort
 // 选择排序
 // 经典排序算法. 遍历数组, 选择最小值j, 交换j与i(i为当前循环所指位置)
-// 遍历一次后即可将
+// 遍历一次后即可将数组排序
 func SelectSort(nums []int) {
 	for i := 0; i < len(nums); i++ {
 		min := nums[i]
